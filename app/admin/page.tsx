835ce2207acf367b78244e51eb68dbab7e17b3f8
AlDoc420 <aledentis@gmail.com>
2026-07-24 04:08:10 +0200

    Protect central barber agenda

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,20 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { AdminDashboard } from "../components/admin-dashboard";
+import {
+  ADMIN_SESSION_COOKIE,
+  verifyAdminSessionToken
+} from "../lib/admin-auth";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const cookieStore = await cookies();
+  if (
+    !verifyAdminSessionToken(
+      cookieStore.get(ADMIN_SESSION_COOKIE)?.value
+    )
+  ) {
+    redirect("/admin/login");
+  }
+
   return <AdminDashboard />;
 }
